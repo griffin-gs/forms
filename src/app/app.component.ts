90d76aac83a206addc5b3d2a772aeb276d37ac5e
Griffin Carr <griffin@goodshuffle.com>
2024-05-13 11:40:07 -0400

    completed template driven forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,60 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FormsModule, NgForm} from "@angular/forms";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, NgIf, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'forms';
+  defaultQuestion: string = 'teacher';
+  answer: string = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
+  submitted = false;
 
-  onSubmit(form: NgForm): void {
-    console.log(form);
+  @ViewChild('f') signupForm!: NgForm;
+
+  suggestUsername() {
+    const suggestedUsername = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedUsername,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedUsername
+      }});
+  }
+
+  // onSubmit(form: NgForm): void {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
